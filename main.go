5d1c3f63a96e48ba75f1a014d1977bfd8db14cc2
1garo <alevardai@hotmail.com>
2021-01-12 20:36:13 -0300

    feat: add new routes

--- main.go
+++ main.go
@@ -7,23 +7,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TODO: implement the concept of pub/sub
-// pub being who post the msg
-// sub being the consumer
+/*
+TODO: implement the concept of pub/sub
+- pub being who post the msg
+- sub being the consumer
+*/
 
 func main() {
-
-	// Dequeue
-	//front := queue.Front()
-	//fmt.Println(front.Value)
-	//fmt.Printf("%v", queue.Back().Value)
-	//// This will remove the allocated memory and avoid memory leaks
-	//queue.Remove(front)
 	r := gin.Default()
 	_ = r.Group("queue")
 	{
 		r.GET("/create", ctl.CreateQueue)
 		r.GET("/populate", ctl.PopulateQueues)
+		r.GET("/dqueue", ctl.RemoveElemFromQueue)
+		r.GET("/dqueueLast", ctl.RemoveLastElemFromQueue)
+		r.GET("/remove", ctl.RemoveElemFromQueue)
 		r.POST("/query", ctl.GetQueues)
 	}
 	err := r.Run()
