187dd8bf7c4f8353f00020846da4f2e107da1762
1garo <alevardai@hotmail.com>
2021-01-13 18:24:57 -0300

    feat: implement remove from top

--- controller/controllers.go
+++ controller/controllers.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -19,19 +18,28 @@ var queue []qMessage = nil
 
 func enqueue(queue []qMessage, element qMessage) []qMessage {
 	queue = append(queue, element)
-	fmt.Println("Enqueued:", element)
+	log.Println("Enqueued: ", element)
 	return queue
 }
 
-func dequeueTop(queue []qMessage) []qMessage { return []qMessage{} }
+func dequeueTop(queue []qMessage) []qMessage {
+	size := len(queue) - 1
+	element := queue[size]
+	// this garbage collector collect this
+	// and not leak memory
+	queue[size] = qMessage{}
+	log.Println("Dequeued from top: ", element)
+	return queue[:size-1]
+}
 
 func dequeueBottom(queue []qMessage) []qMessage {
 	element := queue[0]
 	queue[0] = qMessage{}
-	fmt.Println("Dequeued:", element)
+	log.Println("Dequeued from bottom: ", element)
 	return queue[1:]
 }
 
+// CreateQueue -> create empty queue object
 func CreateQueue(c *gin.Context) {
 	queue = make([]qMessage, 0)
 	c.JSON(http.StatusOK, gin.H{
@@ -39,87 +47,67 @@ func CreateQueue(c *gin.Context) {
 	})
 }
 
-func RemoveElemFromQueue(c *gin.Context) {
+// RemoveElemFromBottom -> remove element from the bottom of the queue
+func RemoveElemFromBottom(c *gin.Context) {
 	if queue == nil {
 		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "queue doesn't exist, please create it!!!",
 		})
 		return
-	} else {
-		queue = dequeueBottom(queue)
-		c.JSON(http.StatusOK, gin.H{
-			"msg": queue,
-		})
 	}
+	queue = dequeueBottom(queue)
+	c.JSON(http.StatusOK, gin.H{
+		"msg": queue,
+	})
 }
 
-// TODO: implement func to remove from top instead of bottom
-func RemoveLastElemFromQueue(c *gin.Context) { dequeueTop([]qMessage{}) }
+// RemoveLastElemFromTop -> remove element from the top of the queue
+func RemoveLastElemFromTop(c *gin.Context) { dequeueTop([]qMessage{}) }
 
+// PopulateQueues -> dummy function to create data for test purpouse
 func PopulateQueues(c *gin.Context) {
 	if queue == nil {
 		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "queue doesn't exist, please create it!!!",
 		})
 		return
-	} else {
-		queue = enqueue(queue, qMessage{
-			USER:  "roberto",
-			EMAIL: "roberto@rr.com",
-			UUID:  "InternalID:1",
-			MSG:   "lindo",
-		})
-		queue = enqueue(queue, qMessage{
-			USER:  "alex",
-			EMAIL: "alex@rr.com",
-			UUID:  "InternalID:2",
-			MSG:   "lindox",
-		})
-		queue = enqueue(queue, qMessage{
-			USER:  "ale",
-			EMAIL: "ale@rr.com",
-			UUID:  "InternalID:3",
-			MSG:   "linduxo",
-		})
-		c.JSON(http.StatusOK, gin.H{
-			"msg": queue,
-		})
 	}
+	queue = enqueue(queue, qMessage{
+		USER:  "roberto",
+		EMAIL: "roberto@rr.com",
+		UUID:  "1",
+		MSG:   "lindo",
+	})
+	queue = enqueue(queue, qMessage{
+		USER:  "alex",
+		EMAIL: "alex@rr.com",
+		UUID:  "2",
+		MSG:   "lindox",
+	})
+	queue = enqueue(queue, qMessage{
+		USER:  "ale",
+		EMAIL: "ale@rr.com",
+		UUID:  "3",
+		MSG:   "linduxo",
+	})
+	c.JSON(http.StatusOK, gin.H{
+		"msg": queue,
+	})
 }
 
+// GetQueues -> return all queues
 func GetQueues(c *gin.Context) {
 	//TODO: create a while both back and front until value is != nil
 	size := len(queue)
 	log.Printf("squeue: %v", queue)
-	// resp := make([]interface{}, size)
 	if size == 0 {
 		c.JSON(http.StatusNotFound, gin.H{
 			"msg": "queue don't have any item!",
 		})
 		return
 	}
-	// valueBack := queue.Back()
-	// if valueBack != nil {
-	// 	resp = append(resp, valueBack.Value)
-	// 	// queue.Remove(valueBack)
-	// } else {
-	// 	c.JSON(http.StatusNotFound, gin.H{
-	// 		"msg": "queue don't have any back item!",
-	// 	})
-	// }
-	// valueFront := queue.Front()
-	// if valueFront != nil {
-	// 	resp = append(resp, valueFront)
-	// 	// queue.Remove(valueFront)
-	// } else {
-	// 	c.JSON(http.StatusNotFound, gin.H{
-	// 		"msg": "queue don't have any front item!",
-	// 	})
-	// }
 	c.JSON(http.StatusOK, gin.H{
 		"queues": queue,
 	})
-	// defer queue.Remove(valueBack)
-	// defer queue.Remove(valueFront)
 	log.Printf("equeue: %v", queue)
 }
