58f158d8617a7632ec30d34a12f057f2b58ad7a2
1garo <alevardai@hotmail.com>
2021-01-26 19:47:52 -0300

    feat: add ws route and websocket handler

---
+++ controller/ws.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// TODO: change this to a websocket server
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Failed to set websocket upgrade: %s\n", err.Error())
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
+}
--- main.go
+++ main.go
@@ -23,6 +23,9 @@ func main() {
 		r.GET("/dqueueLast", ctl.RemoveLastElemFromTop)
 		r.GET("/remove", ctl.RemoveElemFromBottom)
 		r.POST("/query", ctl.GetQueues)
+		r.GET("/ws", func(c *gin.Context) {
+			ctl.WsHandler(c.Writer, c.Request)
+		})
 	}
 	err := r.Run()
 	if err != nil {
